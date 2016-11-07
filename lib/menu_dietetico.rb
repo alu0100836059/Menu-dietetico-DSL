b8fdd909ae7c5962c8865ed281eb6be9d07790ba
alu0100836059 <alu0100836059@ull.edu.es>
2016-11-07 23:05:48 +0000

    Implementada primera prueba: existencia de un nodo

--- lib/menu_dietetico.rb
+++ lib/menu_dietetico.rb
@@ -1,5 +1,74 @@
 require "menu_dietetico/version"
 
 module MenuDietetico
-  # Your code goes here...
-end
+  Node = Struct.new(:value, :next)
+  
+  class Menu_dietetico
+     
+     attr_accessor :head, :num_nodos
+     
+     def initialize
+         @head = Node.new(nil,nil)
+         @num_nodos = 0
+     end
+     
+     def get_num_nodos
+         @num_nodos
+     end
+     
+     
+     def insert(value)
+        if @head.value != nil
+            aux = @head
+            while aux.next != nil
+                aux = aux.next
+            end
+            aux.next = value
+        else
+            @head = value
+        end
+        @num_nodos += 1
+    end
+    
+#     def extract_first
+       
+#       if @head.value == nil
+#           puts "\nLista vacía, nada que extraer\n"
+#       else
+#           aux = @head
+#           @head = aux.next
+#           return aux
+#       end
+#     end
+    
+#     def extract_last
+        
+#         if @head.value == nil
+#             puts "Lista vacía, nada que extraer"
+#         else
+#             aux = @head
+#             while aux.next.next != nil
+#                 aux = aux.next
+#             end
+#             aux_2 = aux.next
+#             aux.next = nil
+#             return aux_2
+#         end
+#     end
+    
+#     def to_s
+#         aux = @head
+#         if aux.value == nil
+#             puts "Lista vacía"
+#         else
+#             while aux.next != nil
+#                 puts aux.value
+#                 aux = aux.next
+#             end
+#         puts aux.value
+#         end
+#     end
+   end #end class
+  
+  
+end #end module
--- spec/menu_dietetico_spec.rb
+++ spec/menu_dietetico_spec.rb
@@ -1,11 +1,31 @@
 require "spec_helper"
+require "menu_dietetico"
+
+# describe MenuDietetico do
+  
+#   it "has a version number" do
+#     expect(MenuDietetico::VERSION).not_to be nil
+#   end
+  
+#   it "does something useful i.e" do
+#     expect(false).to eq(false)
+#   end
+
 
 describe MenuDietetico do
-  it "has a version number" do
-    expect(MenuDietetico::VERSION).not_to be nil
+  before :all do
+    @menu_1 = MenuDietetico::Menu_dietetico.new()
+    @nodo_1 = MenuDietetico::Node.new("Macarrones", nil)
   end
   
-  it "does something useful" do
-    expect(false).to eq(true)
+  
+describe "Debe existir un nodo de la lista con su valor y siguiente" do
+  
+  it "Existe un nodo en la lista" do
+    expect(@menu_1.get_num_nodos).to eq 0
+    @menu_1.insert(@nodo_1)
+    expect(@menu_1.get_num_nodos).to eq 1
   end
+  
+end
 end
