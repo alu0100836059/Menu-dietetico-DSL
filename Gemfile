596d04857365e0a7c0fda074c8abf9733562c6a7
alu0100836059 <alu0100836059@ull.edu.es>
2016-11-07 18:22:20 +0000

    Falta configuración de Guardfile

--- Gemfile
+++ Gemfile
@@ -1,4 +1,7 @@
 source 'https://rubygems.org'
 
 # Specify your gem's dependencies in menu_dietetico.gemspec
+group :development do
+    gem 'guard'
+end
 gemspec
--- spec/menu_dietetico_spec.rb
+++ spec/menu_dietetico_spec.rb
@@ -4,7 +4,7 @@ describe MenuDietetico do
   it "has a version number" do
     expect(MenuDietetico::VERSION).not_to be nil
   end
-
+  
   it "does something useful" do
     expect(false).to eq(true)
   end
